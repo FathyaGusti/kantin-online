4af8f4fbb98c2931cfb6221e49af8451be7f3067
FathyaGusti <161664522+FathyaGusti@users.noreply.github.com>
2024-03-08 14:11:37 +0700

    Update about-us.blade.php

--- resources/views/frontend/pages/about-us.blade.php
+++ resources/views/frontend/pages/about-us.blade.php
@@ -1,6 +1,6 @@
 @extends('frontend.layouts.master')
 
-@section('title','E-SHOP || About Us')
+@section('title','JAVEER || About Us')
 
 @section('main-content')
 
@@ -30,7 +30,7 @@
 							@php
 								$settings=DB::table('settings')->get();
 							@endphp
-							<h3>Welcome To <span>Eshop</span></h3>
+							<h3>Kantin Virtual <span>SMKN 1 CIBINONG</span></h3>
 							<p>@foreach($settings as $data) {{$data->description}} @endforeach</p>
 							<div class="button">
 								<a href="{{route('blog')}}" class="btn">Our Blog</a>
@@ -51,46 +51,73 @@
 	</section>
 	<!-- End About Us -->
 
-
-	<!-- Start Shop Services Area -->
-	<section class="shop-services section">
+	<!--Kantin Menu Section -->
+	<section class="kantin-menu section">
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-3 col-md-6 col-12">
-					<!-- Start Single Service -->
-					<div class="single-service">
-						<i class="ti-rocket"></i>
-						<h4>Free shiping</h4>
-						<p>Orders over $100</p>
+				@foreach ($kantin_menu as $menu )
+				<div class="col-lg-4 col-md-6 col-12">
+					<div class="single-menu">
+						<div class="menu-img">
+							<img src="{{$menu->image }}" alt="{{ $menu->name }}">
 					</div>
-					<!-- End Single Service -->
+					<div class="menu-content">
+						<h4>{{ $menu->name }}</h4>
+						<p>{{ $menu->description }}</p>
+						<span class="price">{{ $menu->price }}</span>
+					</div>
 				</div>
-				<div class="col-lg-3 col-md-6 col-12">
-					<!-- Start Single Service -->
-					<div class="single-service">
-						<i class="ti-reload"></i>
-						<h4>Free Return</h4>
-						<p>Within 30 days returns</p>
-					</div>
-					<!-- End Single Service -->
 				</div>
-				<div class="col-lg-3 col-md-6 col-12">
-					<!-- Start Single Service -->
-					<div class="single-service">
-						<i class="ti-lock"></i>
-						<h4>Sucure Payment</h4>
-						<p>100% secure payment</p>
+				@endforeach
+			</div>
+		</div>
+	</section>
+
+
+		<!-- Start Shop Services Area  -->
+		<section class="shop-services section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-6 col-12">
+						<!-- Start Single Service -->
+						<div class="single-service">
+							<i class="ti-hand-drag"></i>
+							<h4>Pembayaran Tunai</h4>
+							<p>Pembayaran dilakukan secara tunai saat menerima pesanan</p>
+						</div>
+						<!-- End Single Service -->
 					</div>
-					<!-- End Single Service -->
+					<div class="col-lg-3 col-md-6 col-12">
+						<!-- Start Single Service -->
+						<div class="single-service">
+							<i class="ti-money"></i>
+							<h4>Pengembalian Makanan Minuman Kadaluarsa</h4>
+							<p>Pengembalian uang makanan,minuman yang kadaluarsa</p>
+						</div>
+						<!-- End Single Service -->
+					</div>
+					<div class="col-lg-3 col-md-6 col-12">
+						<!-- Start Single Service -->
+						<div class="single-service">
+							<i class="ti-check"></i>
+							<h4>Pilihan Makanan Minuman Sehat</h4>
+							<p>Makanan, Minuman Sehat dan Berkualitas</p>
+						</div>
+						<!-- End Single Service -->
+					</div>
+					<div class="col-lg-3 col-md-6 col-12">
+						<!-- Start Single Service -->
+						<div class="single-service">
+							<i class="ti-tag"></i>
+							<h4>Harga Terbaik</h4>
+							<p>Harga tejangkau untuk semua </p>
+						</div>
+						<!-- End Single Service -->
+					</div>
 				</div>
-				<div class="col-lg-3 col-md-6 col-12">
-					<!-- Start Single Service -->
-					<div class="single-service">
-						<i class="ti-tag"></i>
-						<h4>Best Peice</h4>
-						<p>Guaranteed price</p>
-					</div>
-					<!-- End Single Service -->
+			</div>
+		</section>
+		<!-- End Shop Newsletter -->
 				</div>
 			</div>
 		</div>
