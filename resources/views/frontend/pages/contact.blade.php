8407a0960bf89dc101f8de3d94c9f8ec8d5cee64
FathyaGusti <161664522+FathyaGusti@users.noreply.github.com>
2024-03-08 14:04:49 +0700

    Update contact.blade.php

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -29,45 +29,45 @@
 									@php
 										$settings=DB::table('settings')->get();
 									@endphp
-									<h4>Get in touch</h4>
-									<h3>Write us a message @auth @else<span style="font-size:12px;" class="text-danger">[You need to login first]</span>@endauth</h3>
+									<h4>Hubungi Kami</h4>
+									<h3>Kirimkan Pesan Kepada Kami @auth @else<span style="font-size:12px;" class="text-danger">[Login Terlebih Dahulu]</span>@endauth</h3>
 								</div>
 								<form class="form-contact form contact_form" method="post" action="{{route('contact.store')}}" id="contactForm" novalidate="novalidate">
 									@csrf
 									<div class="row">
 										<div class="col-lg-6 col-12">
 											<div class="form-group">
-												<label>Your Name<span>*</span></label>
-												<input name="name" id="name" type="text" placeholder="Enter your name">
+												<label>Nama<span>*</span></label>
+												<input name="nama" id="nama" type="text" placeholder="Masukkan Nama Anda">
 											</div>
 										</div>
 										<div class="col-lg-6 col-12">
 											<div class="form-group">
-												<label>Your Subjects<span>*</span></label>
-												<input name="subject" type="text" id="subject" placeholder="Enter Subject">
+												<label>Jurusan<span>*</span></label>
+												<input name="jurusan" type="text" id="jurusan" placeholder="Masukkan Jurusan Anda">
 											</div>
 										</div>
 										<div class="col-lg-6 col-12">
 											<div class="form-group">
-												<label>Your Email<span>*</span></label>
-												<input name="email" type="email" id="email" placeholder="Enter email address">
+												<label>Email<span>*</span></label>
+												<input name="email" type="email" id="email" placeholder="Masukkan Email Anda">
 											</div>	
 										</div>
 										<div class="col-lg-6 col-12">
 											<div class="form-group">
-												<label>Your Phone<span>*</span></label>
-												<input id="phone" name="phone" type="number" placeholder="Enter your phone">
+												<label>Nomor Telepon<span>*</span></label>
+												<input id="nomortelepon" name="nomortelepon" type="number" placeholder="Masukkan Nomor Telepon Anda">
 											</div>	
 										</div>
 										<div class="col-12">
 											<div class="form-group message">
-												<label>your message<span>*</span></label>
-												<textarea name="message" id="message" cols="30" rows="9" placeholder="Enter Message"></textarea>
+												<label>Pesan<span>*</span></label>
+												<textarea name="pesan" id="pesan" cols="30" rows="9" placeholder="Masukkan Pesan Anda "></textarea>
 											</div>
 										</div>
 										<div class="col-12">
 											<div class="form-group button">
-												<button type="submit" class="btn ">Send Message</button>
+												<button type="submit" class="btn ">Kirim Pesan</button>
 											</div>
 										</div>
 									</div>
@@ -78,23 +78,31 @@
 							<div class="single-head">
 								<div class="single-info">
 									<i class="fa fa-phone"></i>
-									<h4 class="title">Call us Now:</h4>
+									<h4 class="title">Hubungi Kami Segera:</h4>
 									<ul>
-										<li>@foreach($settings as $data) {{$data->phone}} @endforeach</li>
+										@foreach($settings as $data)
+										<li>
+											<a href="tel:>phone}}">{{$data->phone}}</a>
+												 @endforeach</li>
+											
 									</ul>
 								</div>
 								<div class="single-info">
 									<i class="fa fa-envelope-open"></i>
 									<h4 class="title">Email:</h4>
 									<ul>
-										<li><a href="mailto:info@yourwebsite.com">@foreach($settings as $data) {{$data->email}} @endforeach</a></li>
+										@foreach($settings as $data) 
+										<li><a href="mailto:{{$data->email}}">{{$data->email}}</a></li>
+									  @endforeach
 									</ul>
 								</div>
 								<div class="single-info">
 									<i class="fa fa-location-arrow"></i>
-									<h4 class="title">Our Address:</h4>
+									<h4 class="title">Alamat Kami:</h4>
 									<ul>
-										<li>@foreach($settings as $data) {{$data->address}} @endforeach</li>
+										>@foreach($settings as $data) 
+										<li><a href="https://www.google.com/maps/search/?api=1&query={{$data->latitude}},{{$data->longitude}}">{[$data->address]}</a></li>										
+										 @endforeach
 									</ul>
 								</div>
 							</div>
@@ -108,7 +116,7 @@
 	<!-- Map Section -->
 	<div class="map-section">
 		<div id="myMap">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14130.857353934944!2d85.36529494999999!3d27.6952226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sne!2snp!4v1595323330171!5m2!1sne!2snp" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.996916726551!2d106.80555790568155!3d-6.522070587491063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c3db9bbedcc3%3A0x1f5280e86053b1e9!2sSMK%20NEGERI%201%20Cibinong!5e0!3m2!1sid!2sid!4v1709113267304!5m2!1sid!2sid" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 		</div>
 	</div>
 	<!--/ End Map Section -->
@@ -121,13 +129,13 @@
 		<div class="modal-dialog" role="document">
 		  <div class="modal-content">
 			<div class="modal-header">
-				<h2 class="text-success">Thank you!</h2>
+				<h2 class="text-success">Terima Kasih !!</h2>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
 			<div class="modal-body">
-				<p class="text-success">Your message is successfully sent...</p>
+				<p class="text-success">Pesan Anda Berhasil Masuk...</p>
 			</div>
 		  </div>
 		</div>
@@ -138,13 +146,13 @@
 		<div class="modal-dialog" role="document">
 		  <div class="modal-content">
 			<div class="modal-header">
-				<h2 class="text-warning">Sorry!</h2>
+				<h2 class="text-warning">Maaf</h2>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
 			<div class="modal-body">
-				<p class="text-warning">Something went wrong.</p>
+				<p class="text-warning">Oops,Terjadi Kesalahan.</p>
 			</div>
 		  </div>
 		</div>
