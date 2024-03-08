c744967b0bbca6b2f850a2e8445f55af5fb9ecca
FathyaGusti <161664522+FathyaGusti@users.noreply.github.com>
2024-03-08 13:51:59 +0700

    Update wishlist.blade.php

--- resources/views/frontend/pages/wishlist.blade.php
+++ resources/views/frontend/pages/wishlist.blade.php
@@ -27,10 +27,10 @@
 					<table class="table shopping-summery">
 						<thead>
 							<tr class="main-hading">
-								<th>PRODUCT</th>
-								<th>NAME</th>
-								<th class="text-center">TOTAL</th> 
-								<th class="text-center">ADD TO CART</th> 
+								<th>KATEGORI</th>
+								<th>NAMA</th>
+								<th class="text-center">JUMLAH</th> 
+								<th class="text-center">MASUKKAN KE KERANJANG</th> 
 								<th class="text-center"><i class="ti-trash remove-icon"></i></th>
 							</tr>
 						</thead>
@@ -46,7 +46,7 @@
 											<p class="product-name"><a href="{{route('product-detail',$wishlist->product['slug'])}}">{{$wishlist->product['title']}}</a></p>
 											<p class="product-des">{!!($wishlist['summary']) !!}</p>
 										</td>
-										<td class="total-amount" data-title="Total"><span>${{$wishlist['amount']}}</span></td>
+										<td class="total-amount" data-title="Total"><span>Rp{{ number_format($wishlist['amount'] * $exchange_rate, 0, ',', '.') }}</span></td>
 										<td><a href="{{route('add-to-cart',$wishlist->product['slug'])}}" class='btn text-white'>Add To Cart</a></td>
 										<td class="action" data-title="Remove"><a href="{{route('wishlist-delete',$wishlist->id)}}"><i class="ti-trash remove-icon"></i></a></td>
 									</tr>
@@ -54,7 +54,7 @@
 							@else 
 								<tr>
 									<td class="text-center">
-										There are no any wishlist available. <a href="{{route('product-grids')}}" style="color:blue;">Continue shopping</a>
+										Tidak ada daftar keinginan yang tersedia. <a href="{{route('product-grids')}}" style="color:rgb(255, 0, 0);">Lanjutkan Membeli</a>
 
 									</td>
 								</tr>
@@ -77,27 +77,27 @@
 				<div class="col-lg-3 col-md-6 col-12">
 					<!-- Start Single Service -->
 					<div class="single-service">
-						<i class="ti-rocket"></i>
-						<h4>Free shiping</h4>
-						<p>Orders over $100</p>
+						<i class="ti-hand-drag"></i>
+						<h4>Pembayaran Tunai</h4>
+						<p>Pembayaran dilakukan secara tunai saat menerima pesanan</p>
 					</div>
 					<!-- End Single Service -->
 				</div>
 				<div class="col-lg-3 col-md-6 col-12">
 					<!-- Start Single Service -->
 					<div class="single-service">
-						<i class="ti-reload"></i>
-						<h4>Free Return</h4>
-						<p>Within 30 days returns</p>
+						<i class="ti-money"></i>
+						<h4>Pengembalian Makanan Minuman Kadaluarsa</h4>
+						<p>Pengembalian Uang Makanan , Minuman yang kadaluarsa</p>
 					</div>
 					<!-- End Single Service -->
 				</div>
 				<div class="col-lg-3 col-md-6 col-12">
 					<!-- Start Single Service -->
 					<div class="single-service">
-						<i class="ti-lock"></i>
-						<h4>Sucure Payment</h4>
-						<p>100% secure payment</p>
+						<i class="ti-check"></i>
+						<h4>Pilihan Makanan Minuman Sehat</h4>
+						<p>Makanan,Minuman Sehat dan berkualitas</p>
 					</div>
 					<!-- End Single Service -->
 				</div>
@@ -105,8 +105,8 @@
 					<!-- Start Single Service -->
 					<div class="single-service">
 						<i class="ti-tag"></i>
-						<h4>Best Peice</h4>
-						<p>Guaranteed price</p>
+						<h4>Harga Terbaik</h4>
+						<p> Harga Terjangkau untuk Semua</p>
 					</div>
 					<!-- End Single Service -->
 				</div>
@@ -214,7 +214,7 @@
 										<a href="#" class="btn min"><i class="ti-heart"></i></a>
 										<a href="#" class="btn min"><i class="fa fa-compress"></i></a>
 									</div>
-                                    <div class="default-social">
+                                     {{-- <div class="default-social">
 										<h4 class="share-now">Share:</h4>
                                         <ul>
                                             <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
@@ -222,7 +222,7 @@
                                             <li><a class="youtube" href="#"><i class="fa fa-pinterest-p"></i></a></li>
                                             <li><a class="dribbble" href="#"><i class="fa fa-google-plus"></i></a></li>
                                         </ul>
-                                    </div>
+                                    </div>  --}}
                                 </div>
                             </div>
                         </div>
